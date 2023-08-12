bfebff1f735b16caf4ef591296bda14bff8f43e2
hekui <1287255589@qq.com>
2023-08-13 00:29:14 +0800

    🌈 style: eslint优化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,9 @@ module.exports = {
   'extends': [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:vue/vue3-essential'
+    'plugin:vue/vue3-essential',
+    '@vue/typescript/recommended',
+    '@vue/eslint-config-typescript'
   ],
   'overrides': [
     {
@@ -36,6 +38,14 @@ module.exports = {
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
     'vue/multi-word-component-names': 'off',
+    'no-multi-spaces': 2,
+    'no-trailing-spaces': 2,
+    'arrow-spacing': ['error', {
+      before: true,
+      after: true
+    }],
+    'space-infix-ops': ['error', { 'int32Hint': false }],
+    'object-curly-spacing': ['error', 'always'],
     'no-unused-vars': [
       'warn',
       { varsIgnorePattern: '.*', args: 'none' }
