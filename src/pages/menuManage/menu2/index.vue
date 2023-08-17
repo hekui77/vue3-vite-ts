166992cb3f09309cd0efeafe3a2880c484d4ab42
hekui <1287255589@qq.com>
2023-08-17 20:28:41 +0800

    ✨ feat: 多级路由

---
+++ src/pages/menuManage/menu2/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+const input = ref('');
+</script>
+
+<template>
+  <div class="app-container">
+    <p>menu2</p>
+    <el-input v-model="input" placeholder="Please input" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
